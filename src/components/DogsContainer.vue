66e164d6f32e07c78091d776904a1da61ef5236f
favourwright <gismozfexie@yahoo.com>
2022-08-01 03:03:39 +0100

    feat: add card skeleton

--- src/components/DogsContainer.vue
+++ src/components/DogsContainer.vue
@@ -4,11 +4,15 @@
   <section class="overflow-hidden text-gray-700">
     <div class="pt-2 px-[6%]">
       <div class="flex flex-wrap md:gap-0 bg-tertiary/10 rounded-2xl">
-        <h1 class="text-xl text-red-500" v-if="fetchingDogs">fetching</h1>
-        <DogCard
-          v-for="dog in dogs"
-          :key="dog"
-          :source="dog" />
+        <template v-if="fetchingDogs">
+          <CardSkeleton v-for="n in 6" :key="n" />
+        </template>
+        <template v-else>
+          <DogCard
+            v-for="dog in dogs"
+            :key="dog"
+            :source="dog" />
+        </template>
       </div>
     </div>
   </section>
@@ -20,6 +24,7 @@ import { computed } from 'vue'
 import { useStore } from 'vuex'
 import DogCard from './DogCard.vue'
 import Customization from './Customization.vue';
+import CardSkeleton from './atoms/CardSkeleton.vue';
 
 const store = useStore()
 store.dispatch('fetchRandomDogs', store.state.fetchLimit) // fetch on created
---
+++ src/components/atoms/CardSkeleton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="w-1/2 md:w-1/3 h-52 p-1 md:p-2">
+    <div class="bg-tertiary/5 h-full w-full rounded-xl md:rounded-lg">
+
+    </div>
+  </div>
+</template>

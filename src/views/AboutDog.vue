e097ef7905a920c579b56bb33d26246818ee1aec
favourwright <gismozfexie@yahoo.com>
2022-08-01 10:32:08 +0100

    chore: cleanup code
    note: last fix didn't work as expected

--- src/views/AboutDog.vue
+++ src/views/AboutDog.vue
@@ -2,7 +2,7 @@
 <main class="min-h-screen">
   <section class="landing w-full">
     <img
-      class="w-full h-full object-cover object-top md:object-center"
+      class="w-full h-full object-cover object-top"
       :src="`https://images.dog.ceo/breeds/${breed}/${imgName}.${imgExt}`"
       :alt="`dog breed ${breed}`">
   </section>
@@ -14,19 +14,20 @@
 
 <script setup>
 import { useRouter, useRoute } from 'vue-router'
-import { ref, onActivated } from 'vue'
+import { ref, computed, onActivated } from 'vue'
 const router = useRouter()
 const route = useRoute()
 
+const getExt = computed(() => !!route.query.ext? route.query.ext : 'jpg')
 const imgName = ref(route.params.id)
-const imgExt = ref(route.query.ext)
+const imgExt = ref(getExt.value)
 const breed = ref(route.query.breed.split(' ').join('-'))
 const breedName = ref(route.query.breed)
 
 // because of keep-alive, we need to manually update the image name
 onActivated(() => {
   imgName.value = route.params.id
-  imgExt.value = route.query.ext
+  imgExt.value = getExt.value
   breed.value = route.query.breed.split(' ').join('-')
   breedName.value = route.query.breed
 })

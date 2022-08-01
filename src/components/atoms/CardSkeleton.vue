f056b9ffaa5fac62edcb8947cd9b305d147e7fc5
favourwright <gismozfexie@yahoo.com>
2022-08-01 03:12:06 +0100

    chore: mod skeleton

--- src/components/atoms/CardSkeleton.vue
+++ src/components/atoms/CardSkeleton.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-1/2 md:w-1/3 h-52 p-1 md:p-2">
+    class="w-1/2 md:w-1/3 h-52 md:h-96 p-1 md:p-2">
     <div class="bg-tertiary/5 h-full w-full rounded-xl md:rounded-lg">
 
     </div>

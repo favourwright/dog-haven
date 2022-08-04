a09a9715dfa06ff0a491140f41e9b4041e050340
favourwright <gismozfexie@yahoo.com>
2022-08-04 15:08:34 +0100

    feat: add next,prev icons

--- src/components/CarouselControls.vue
+++ src/components/CarouselControls.vue
@@ -8,19 +8,31 @@
         <div class="order-last">0{{slides_count}}</div>
       </template>
       <AutoPlayProgress
-        class="w-full"
+        class="w-full h-[2px]"
         :play="play"
         :duration="duration" />
     </div>
-    <div
-      class="seek pr-6 flex justify-center items- h-full">
-      <!-- emit seek event -->
+    <div class="seek pr-6 flex justify-center items- h-full">
       <button
         @click="emit('seek', -1)"
-        class="px-4 flex items-center">[</button>
+        class="px-1 flex items-center">
+        <svg
+          class="w-4 h-4 fill-white"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512">
+          <path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/>
+        </svg>
+      </button>
       <button
         @click="emit('seek', 1)"
-        class="px-4 flex items-center">]</button>
+        class="px-4 flex items-center">
+        <svg
+          class="w-4 h-4 fill-white"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 320 512">
+          <path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/>
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -52,9 +64,4 @@ const props = defineProps({
     required: true
   }
 })
-
 </script>
-
-<style scoped>
-
-</style>

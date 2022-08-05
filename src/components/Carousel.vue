bfce82c5f3cc389d9d859b5677b2895cc243f988
favourwright <gismozfexie@yahoo.com>
2022-08-05 22:04:09 +0100

    feat: calc slide gaps/gutters value & keep copy of original slides position before ever sliding them

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,11 +4,15 @@
       class="w-full md:w-[110%] overflow-hidden">
       <div class="slides flex justify-start items-end flex-row-reverse gap-3">          
         <div
-          v-for="n in slides" :key="n"
-          class="flex-none card w-32 h-48">
+          v-for="(slide,n) in slides" :key="slide.id"
+          :class="[(n===0 || n===1)? 'bigger' : '']"
+          class="flex-none card w-32 h-48"
+          :data-original-index="n"
+          :data-original-id="slide.id"
+          ref="card">
           <img
             class="w-full h-full object-cover"
-            src='https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+            :src='slide.src'
             alt=''>
         </div>
       </div>
@@ -17,15 +21,15 @@
     <CarouselControls
       @seek="HandleSeek"
       :play="true"
-      :duration="3000"
+      :duration="5000"
       :current="current"
-      :slides_count="5"
+      :slides_count="6"
       class="px-8 h-16"/>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import CarouselControls from './CarouselControls.vue';
 // I could emmit the current slide number to the hero page later
 
@@ -40,26 +44,83 @@ const HandleSeek = (direction) => {
   }
 }
 
-const slides = [1,2,3,4,5,6]
+const slides = [
+  {
+    id: '05700d52f4d9',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  },
+  {
+    id: '5d5d9e9c8b9c',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  },
+  {
+    id: '5d5d9edddvb9c',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  },
+  {
+    id: '5ddfbdsnh8b9c',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  },
+  {
+    id: '5dadfgebvc8bc',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  },
+  {
+    id: '5d5d8b9cskhdk',
+    src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
+  }
+]
+const original_card_details = ref([])
+const card_details = ref([])
+// this ref contains all the card elements
+const card = ref(null)
+onMounted(()=>console.log(card.value))
+
+// next(){
+//   this.active_index = Math.abs((this.active_index + 1) % this.testimonials.length)
+//   // clearing intervals so that autoplaying would reset
+//   this.pauseAutoPlay()
+//   this.autoPlay()
+// },
+// prev(){
+//   this.active_index = Math.abs((this.active_index - 1) % this.testimonials.length)
+//   this.pauseAutoPlay()
+//   this.autoPlay()
+// },
+// seek(index){
+//   this.active_index = index
+//   this.pauseAutoPlay()
+//   this.autoPlay()
+// },
+// autoPlay(){
+//   this.auto_play_interval = setInterval(()=>{
+//     this.next()
+//   }, this.slide_duration)
+// },
+// pauseAutoPlay(){
+//   clearInterval(this.auto_play_interval)
+// },
+
 // width of first & second child are same but different from the rest
+
 const large_card_width = ref(null)
 onMounted(()=>large_card_width.value = document.querySelector('.card').getBoundingClientRect().width+'px')
-
-const card_details = ref([])
-// update array of cards widths
-const updateCardDetails = detail => {
-  card_details.value = [
-    ...card_details.value,
-    detail
-  ]
-}
-// update -- data-number affter each unshift() & push()
+const card_gap = computed(()=>{
+  // minus the position of the last card from the position of the first
+  const container_size = Math.abs(card_details.value.slice(-1)[0].right - card_details.value[0].right)
+  // minus each card width from the container size the divide by the number of cards
+  return Math.abs(
+    (container_size - card_details.value.reduce(
+      (acc, curr)=>acc + curr.width, 0)
+    ) / card_details.value.length
+  ).toFixed(3)+'px'
+})
 
 const CreateIntersectionObserver = (root, target, callback)=>{
   let options = {
     root: document.querySelector(root),
     rootMargin: '0px',
-    threshold: [0, 1.0]
+    threshold: [0, 1.0] // only 1.0 is needed to trigger the callback
   }
   let observer = new IntersectionObserver(callback, options)
   document.querySelectorAll(target).forEach(el => observer.observe(el))
@@ -71,14 +132,14 @@ onMounted(()=>{
     entries.forEach((entry) => {
       const { right, width } = entry.boundingClientRect
       const ratio = entry.intersectionRatio
-      const isIntersecting = entry.isIntersecting 
-      updateCardDetails({ right, width, ratio, isIntersecting })
-      //   entry.intersectionRect
-      //   entry.rootBounds
-      //   entry.target
-      //   entry.time
+      const isIntersecting = entry.isIntersecting
+      // update array of cards widths
+      card_details.value = [ ...card_details.value, { right, width, ratio, isIntersecting } ]
     })
-    console.log(card_details.value);
+    // make sure the value is updated only once
+    if(original_card_details.value.length === 0){
+      original_card_details.value = [ ...card_details.value ]
+    }
   })
 })
 </script>
@@ -88,8 +149,9 @@ onMounted(()=>{
 .slides{
   transform: translateX(v-bind(large_card_width))
 }
-.slides > .card:nth-of-type(1),
-.slides > .card:nth-of-type(2){
+
+.slides > .card:nth-of-type(1).bigger,
+.slides > .card:nth-of-type(2).bigger{
   @apply h-56 w-40
 }
 </style>

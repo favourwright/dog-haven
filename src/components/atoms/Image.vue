cc024e7ff8ebd253af3e880ecf45cf6316b59378
favourwright <gismozfexie@yahoo.com>
2022-08-06 00:28:40 +0100

    chore: reverted to vue-use because its more concise

--- src/components/atoms/Image.vue
+++ src/components/atoms/Image.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="relative" :class="$attrs.class">
-    <CardSkeleton style="width:100%" v-if="!source_available && isLoading" />
-    <img v-if="source_available && !isLoading" :class="$attrs.class" :src="source">
-    <img
-      v-if="source_available"
-      :src="source"
-      :class="img_name"
-      class="h-0 w-0">
-  </div>
+  <CardSkeleton style="width:100%" v-if="!source_available && isLoading" />
+  <img :class="$attrs.class" v-else :src="source">
 </template>
 
 <script setup>
+import { useImage } from '@vueuse/core'
 import { computed, ref, watch } from 'vue';
 import CardSkeleton from './CardSkeleton.vue';
 
@@ -24,26 +18,11 @@ const props = defineProps({
 const src = computed(() => props.source)
 const source_available = ref(false)
 const isLoading = ref(true)
-
-// using regexp replace all symbols and keep only text
-const img_name = computed(() => src.value.replace(/[^a-zA-Z0-9]/g, ''))
-
 const unwatch = watch(src, (new_source) => {
   if(new_source!==''){
     source_available.value = true
-    isLoadingHandler()
+    isLoading.value = useImage({src: new_source}).isLoading
+    unwatch() // cleanup
   }
 }, { immediate: true })
-
-const isLoadingHandler = () => {
-  // run in nextTick because of errors in console
-  Promise.resolve().then(() => {
-    const fake = document.querySelector(`.${img_name.value}`)
-    fake.addEventListener('load', () => {
-      console.log('loaded: '+img_name.value);
-      isLoading.value = false
-      unwatch() // cleanup
-    })
-  })
-}
 </script>

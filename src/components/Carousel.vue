b24eb1f4b40448fc6692554bfbea5b58d761fc5a
favourwright <gismozfexie@yahoo.com>
2022-08-06 15:39:02 +0100

    feat: buggy slides translation

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -9,7 +9,7 @@
           class="flex-none card w-32 h-48"
           :data-original-index="n"
           :data-original-id="slide.id"
-          ref="card">
+          ref="cards">
           <img
             class="w-full h-full object-cover"
             :src='slide.src'
@@ -34,18 +34,11 @@ import CarouselControls from './CarouselControls.vue';
 // I could emmit the current slide number to the hero page later
 
 const current = ref(1)
-const HandleSeek = (direction) => {
-  direction === -1 ? prev() : next()
-  console.log(current.value)
-}
-// calculate current value noting that once we reach the end we start from the beginning
-const next = () => {
-  current.value = current.value === slides.length ? 1 : current.value + 1
-}
-const prev = () => {
-  current.value = current.value === 1 ? slides.length : current.value - 1
-}
-
+const original_card_details = ref([])
+const card_details = ref([])
+// this ref contains all the card elements
+const cards = ref(null)
+const slide_duration = 300
 const slides = [
   {
     id: '05700d52f4d9',
@@ -72,11 +65,44 @@ const slides = [
     src: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80'
   }
 ]
-const original_card_details = ref([])
-const card_details = ref([])
-// this ref contains all the card elements
-const card = ref(null)
-// onMounted(()=>console.log(card.value))
+
+const large_card_width = ref(null)
+onMounted(()=>large_card_width.value = document.querySelector('.card').getBoundingClientRect().width+'px')
+const card_gap = computed(()=>{
+  // minus the position of the last card from the position of the first
+  const container_size = Math.abs(card_details.value.slice(-1)[0].left - card_details.value[0].left)
+  // minus each card width from the container size the divide by the number of cards
+  return Math.ceil(Math.abs(
+    (container_size - card_details.value.reduce(
+      (acc, curr)=>acc + curr.width, 0)
+    ) / card_details.value.length
+  )).toFixed(3)
+})
+
+const HandleSeek = (direction) => {
+  direction === -1 ? prev() : next()
+}
+// calculate current value noting that once we reach the end we start from the beginning
+const next = () => {
+  current.value = current.value === slides.length ? 1 : current.value + 1
+  move('+')
+}
+const prev = () => {
+  current.value = current.value === 1 ? slides.length : current.value - 1
+  move('-')
+}
+const move = direction => {
+  // translate all cards by their width
+  cards.value.forEach((card,i) => {
+    card.style.transition = `all ${slide_duration}ms ease-in-out`
+    // console.log(`${direction + (card_details.value[i].width + +card_gap.value)}px`);
+    card.style.transform = `translateX(calc((100% + ${card_gap.value}px) * ${direction}1))`
+    // card.style.transform = `translateX(${+(direction+1) * (card_details.value[i].width + +card_gap.value)}px)`
+    setTimeout(() => {
+      // clear the transition after the transition is over
+    }, slide_duration)
+  })
+}
 
 // next(){
 //   this.active_index = Math.abs((this.active_index + 1) % this.testimonials.length)
@@ -105,24 +131,11 @@ const card = ref(null)
 
 // width of first & second child are same but different from the rest
 
-const large_card_width = ref(null)
-onMounted(()=>large_card_width.value = document.querySelector('.card').getBoundingClientRect().width+'px')
-const card_gap = computed(()=>{
-  // minus the position of the last card from the position of the first
-  const container_size = Math.abs(card_details.value.slice(-1)[0].right - card_details.value[0].right)
-  // minus each card width from the container size the divide by the number of cards
-  return Math.abs(
-    (container_size - card_details.value.reduce(
-      (acc, curr)=>acc + curr.width, 0)
-    ) / card_details.value.length
-  ).toFixed(3)+'px'
-})
-
 const CreateIntersectionObserver = (root, target, callback)=>{
   let options = {
     root: document.querySelector(root),
     rootMargin: '0px',
-    threshold: [0, 1.0] // only 1.0 is needed to trigger the callback
+    threshold: [0, 0.5, 1]
   }
   let observer = new IntersectionObserver(callback, options)
   document.querySelectorAll(target).forEach(el => observer.observe(el))
@@ -132,12 +145,15 @@ onMounted(()=>{
   CreateIntersectionObserver('.carousel', '.card', (entries, observer)=>{
     card_details.value=[] // clear previous data
     entries.forEach((entry) => {
-      const { right, width } = entry.boundingClientRect
+      const { left, width } = entry.boundingClientRect
       const ratio = entry.intersectionRatio
       const isIntersecting = entry.isIntersecting
       // update array of cards widths
-      card_details.value = [ ...card_details.value, { right, width, ratio, isIntersecting } ]
+      card_details.value = [ ...card_details.value, { left, width, ratio, isIntersecting } ]
     })
+    console.log(card_details.value);
+
+
     // make sure the value is updated only once
     if(original_card_details.value.length === 0){
       original_card_details.value = [ ...card_details.value ]
